d6ed31347c9787cf98733b3b45148c81d11d4c3f
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-08-12 19:52:43 -0300

    Implementa rascunho de write_char().

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,24 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 10:34:01 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/08/12 19:39:53 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/08/12 19:50:27 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
-#include <string.h>
-#include <stdlib.h>
+
+static int	write_char(char *s)
+{
+	int	count;
+
+	count = 0;
+	while (*s)
+	{
+		count += write(1, *s, 1);
+		s++;
+	}
+	return (count);
+}
 
 char	*ft_printf(const char *s, ...)
 {
--- libftprintf.h
+++ libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 10:39:03 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/08/12 19:35:10 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/08/12 19:47:02 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 # define LIBFTPRINTF_H
 
 # include <stdarg.h>
+# include <stdlib.h>
 
 char	*ft_printf(const char *s, ...);
 
