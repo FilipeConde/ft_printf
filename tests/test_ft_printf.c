b25dae15c2f824cf8da29d7fd742993b08ad1f17
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-08-13 19:55:51 -0300

    Add função que copia conteúdo de arquivo para string para posterior manipulação.

--- tests/test_ft_printf.c
+++ tests/test_ft_printf.c
@@ -2,6 +2,31 @@
 #include "./Unity-2.6.1/src/unity.h"
 #include "../libftprintf.h"
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+char	*read_file_to_str(const char *filename)
+{
+	FILE	*f;
+	long	fsize;
+	char	*str;
+
+	fopen(filename, "rb");
+	if (!f)
+		return (NULL);
+	fseek(f, 0, SEEK_END);
+	fsize = ftell(f);
+	fseek(f, 0, SEEK_SET);
+	str = malloc(fsize + 1);
+	if (!str)
+		return (NULL);
+	fread(str, fsize, 1, f);
+	fclose(f);
+	str[fsize] = '\0';
+	return (str);
+}
+
 void	setUp()
 {
 	return ;
