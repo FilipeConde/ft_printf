dd0a641ff3d38416572103cd83ac3d50503b3102
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-08-13 23:33:47 -0300

    Isola função em pasta 'utils' para reutilização em diferentes arquivos.

---
+++ tests/utils/utils.h
@@ -0,0 +1,12 @@
+
+#ifndef UTILS_H
+# define UTILS_H
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*read_file_to_str(const char *filename);
+
+#endif
