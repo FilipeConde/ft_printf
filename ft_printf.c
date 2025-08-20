be6939d3d9e93f055e418f6da2ab7e44344eaf1a
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-08-20 20:43:31 -0300

    refactor: isola função de write_flag().

--- ft_printf.c
+++ ft_printf.c
@@ -6,12 +6,35 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 10:34:01 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/08/19 22:31:30 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/08/20 20:17:19 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
 #include <stdio.h>
+
+static void	write_flag(const char s, va_list args, int *count)
+{
+	char *s_var;
+	
+	if (s == 'c')
+	{
+		char c_var = va_arg(args, int);
+		*count += ft_putchar_fd(c_var, 1);
+	}
+	if (s == 's')
+	{
+		s_var = va_arg(args, char *);
+		if (!s_var)
+			*count += ft_putstr_fd("(null)", 1);
+		else
+			*count += ft_putstr_fd(s_var, 1);
+	}
+	if (s == 'd' || s == 'i')
+	{
+		*count+= ft_putnbr_fd(va_arg(args, int), 1);
+	}
+}
 
 int	ft_printf(const char *s, ...)
 {
@@ -24,34 +47,15 @@ int	ft_printf(const char *s, ...)
 	va_start(args, s);
 	while (*s)
 	{
-		while (*s)
+		if (*s == '%')
 		{
-			if (*s == '%')
-			{
-				// code to handle format specifier cspdiuxX%
-				s++;
-				if (*s == 'c')
-				{
-					c_var = va_arg(args, int);
-					count += ft_putchar_fd(c_var, 1);
-				}
-				if (*s == 's')
-				{
-					s_var = va_arg(args, char *);
-					if (!s_var)
-						count += ft_putstr_fd("(null)", 1);
-					else
-						count += ft_putstr_fd(s_var, 1);
-				}
-				if (*s == 'd' || *s == 'i')
-				{
-					count+= ft_putnbr_fd(va_arg(args, int), 1);
-				}
-			}
-			else
-				count += write(1, s, 1);
+			// code to handle format specifier cspdiuxX%
 			s++;
+			write_flag(*s, args, &count);
 		}
+		else
+			count += write(1, s, 1);
+		s++;
 	}
 	va_end(args);
 	return (count);
