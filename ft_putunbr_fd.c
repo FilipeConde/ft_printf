3c54ab94c563fc40d3fd89aeb14fc4659780e538
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-08-21 21:35:09 -0300

    add: cria função para escrita de unsigned int.

---
+++ ft_putunbr_fd.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putunbr_fd.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/03 19:12:48 by fconde-p          #+#    #+#             */
+/*   Updated: 2025/08/21 21:08:56 by fconde-p         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+
+int ft_putunbr_fd(unsigned int n, int fd)
+{
+	int count;
+
+	count = 0;
+	if (n >= 10)
+	{
+		count += ft_putunbr_fd(n / 10, fd);
+	}
+	count += ft_putchar_fd((n % 10) + '0', fd);
+	return (count);
+}
