c7868e771301007d53d6073255df20c3d61f68a0
Filipe Conde Pereira <fconde-p@student.42sp.org.br>
2025-08-25 21:07:13 -0300

    refactor: cria função do_flag_p() e do_flag_s() para atender à norma.

--- ft_printf.c
+++ ft_printf.c
@@ -6,29 +6,42 @@
 /*   By: fconde-p <fconde-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/08 10:34:01 by fconde-p          #+#    #+#             */
-/*   Updated: 2025/08/25 19:57:18 by fconde-p         ###   ########.fr       */
+/*   Updated: 2025/08/25 21:05:05 by fconde-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+static void	do_flag_p(unsigned long i_var, int *count)
+{
+	if (!i_var)
+		*count += ft_putstr_fd("(nil)", 1);
+	else
+	{
+		*count += ft_putstr_fd("0x", 1);
+		*count += ft_putulhex(i_var, 'x');
+	}
+}
+
+static void	do_flag_s(char *s_var, int *count)
+{
+	if (!s_var)
+		*count += ft_putstr_fd("(null)", 1);
+	else
+		*count += ft_putstr_fd(s_var, 1);
+}
+
 static void	write_flag(const char s, va_list args, int *count)
 {
-	char	*s_var;
-	unsigned long		i_var;
+	char			*s_var;
+	unsigned long	i_var;
 
 	if (s == '%')
 		*count += ft_putchar_fd('%', 1);
 	if (s == 'p')
 	{
 		i_var = va_arg(args, unsigned long);
-		if (!i_var)
-			*count += ft_putstr_fd("(nil)", 1);
-		else
-		{
-			*count += ft_putstr_fd("0x", 1);
-			*count += ft_putulhex(i_var, 'x');
-		}
+		do_flag_p(i_var, count);
 	}
 	if (s == 'x' || s == 'X')
 		*count += ft_puthex(va_arg(args, unsigned int), s);
@@ -37,10 +50,7 @@ static void	write_flag(const char s, va_list args, int *count)
 	if (s == 's')
 	{
 		s_var = va_arg(args, char *);
-		if (!s_var)
-			*count += ft_putstr_fd("(null)", 1);
-		else
-			*count += ft_putstr_fd(s_var, 1);
+		do_flag_s(s_var, count);
 	}
 	if (s == 'd' || s == 'i')
 		*count += ft_putnbr_fd(va_arg(args, int), 1);
@@ -64,7 +74,6 @@ int	ft_printf(const char *s, ...)
 		}
 		else if (*s == '%' && !*(s + 1))
 			return (count);
-		// count += write(1, s, 1);
 		else
 			count += write(1, s, 1);
 		s++;
